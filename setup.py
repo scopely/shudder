c6dae2e79764bfd9b19691f8707879f7300cb51e
Anthony Grimes <i@raynes.me>
2014-10-09 12:09:45 -0700

    Add license info and clean up old template placeholders.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-"""Your project's description"""
+"""A simple service for capturing autoscaling lifecycle hook actions
+and notifying another service that it needs a graceful shutdown.
+
+"""
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
@@ -6,14 +9,14 @@ with open('requirements.txt') as f:
 
 setup(
     name='shudder',
-    description="A project that does things!",
+    description="Graceful shutdowns using autoscaling lifecycle hooks.",
     version='0.1.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     author='Anthony Grimes',
-    author_email='anemail@raynes.me',
-    url='https://github.com/Raynes/shudder',
-    license='MIT',
+    author_email='anthony@scopely.com',
+    url='https://github.com/scopely/shudder',
+    license='Apache 2.0',
     install_requires=requirements
 )
